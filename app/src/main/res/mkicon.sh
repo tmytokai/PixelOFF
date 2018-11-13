3119fdbe7e285d128b493093c95088972d447a18
tmytokai <tmytokai@yahoo.co.jp>
2018-11-13 18:34:45 +0900

    add screenshot

--- app/src/main/res/mkicon.sh
+++ app/src/main/res/mkicon.sh
@@ -1,6 +1,15 @@
+mkdir ./mipmap-mdpi
 convert -scale  48 ./key.svg ./mipmap-mdpi/key.png
+
+mkdir ./mipmap-hdpi
 convert -scale  72 ./key.svg ./mipmap-hdpi/key.png
+
+mkdir ./mipmap-xhdpi
 convert -scale  96 ./key.svg ./mipmap-xhdpi/key.png
+
+mkdir ./mipmap-xxhdpi
 convert -scale 144 ./key.svg ./mipmap-xxhdpi/key.png
+
+mkdir ./mipmap-xxxhdpi
 convert -scale 192 ./key.svg ./mipmap-xxxhdpi/key.png
 
